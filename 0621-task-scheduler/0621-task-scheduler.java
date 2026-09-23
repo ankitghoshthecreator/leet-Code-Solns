c91ff4bb94bef1ae26a473be6c856a2bf493627b
Ankit Ghosh <142504274+ankitghoshthecreator@users.noreply.github.com>
2026-09-23 21:49:10 +0530

    Time: 1 ms (100%), Space: 48.1 MB (67.73%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq=new int[26];
+
+        for(char task:tasks){
+            freq[task-'A']++;
+        }
+
+        int maxFreq=0;
+
+        for(int f:freq){
+            maxFreq=Math.max(maxFreq, f);
+        }
+
+        int countMax=0;
+
+        for(int f:freq){
+            if(f==maxFreq){
+                countMax++;
+            }
+        }
+        int totalTask=tasks.length;
+        int f=(maxFreq-1)*(n+1)+countMax;
+
+        return Math.max(totalTask, f);
+    }
+}
